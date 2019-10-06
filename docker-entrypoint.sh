8fcc0838a5e3c5e31dcae3c11e08c728d29f865f
Heberson Aguiar <hebersonaguiar@gmail.com>
2019-10-06 20:02:37 -0300

    [MODIFY] Alterando variaveis

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 help() {
     echo "Usage: docker run -dti -e ALLOWED_ORIGIN=<value1> -e REDIS_ADDR=<value2> image:tag" >&2
     echo
-    echo "   ALLOWED_ORIGIN        Origin for alloed connections ex: http://0.0.0.0:3000"
-    echo "   REDIS_ADDR            Redis server ex: 0.0.0.0:6379"
+    echo "   ALLOWED_ORIGIN        Origin for alloed connections ex: frontend.example.com"
+    echo "   REDIS_ADDR            Redis server ex: redis.example.com"
 
     echo
     exit 1
@@ -14,10 +14,19 @@ help() {
 
 if [ ! -z "$ALLOWED_ORIGIN" ] || [ ! -z "$REDIS_ADDR" ] ; then
 
-    echo "export ALLOWED_ORIGIN='$ALLOWED_ORIGIN'" >> ~/.bash_profile
-    echo "export REDIS_ADDR=$REDIS_ADDR" >> ~/.bash_profile
-    export -p
-    source ~/.bash_profile 
+    dig +short $REDIS_ADDR > /app/REDIS_ADDR
+    IPREDIS=$(cat /app/IPBACKEND)
+    sed -i "s/REDIS_ADDR/$IPREDIS/g" /app/main.go
+    sed -i "s/ALLOWED_ORIGIN/$ALLOWED_ORIGIN/g" /app/main.go
+    
+    cd /app
+    go get ./...
+    go build ./
+
+    #echo "export ALLOWED_ORIGIN='$ALLOWED_ORIGIN'" >> ~/.bash_profile
+    #echo "export REDIS_ADDR=$REDIS_ADDR" >> ~/.bash_profile
+    #export -p
+    #source ~/.bash_profile 
 
 else
 	echo "Please enter the required variables!"
